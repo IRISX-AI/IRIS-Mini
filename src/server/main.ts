581820de63b4c891b4e90efdfc9c2365c93bf97b
Harsh <pandeygautam039@gmail.com>
2026-05-09 20:58:30 +0530

    build(deps): update package-lock.json, package.json and main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,8 @@
 import "../config/dot-env.js";
 import express from "express";
 import ViteExpress from "vite-express";
+import chalk from "chalk";
+import { getAvailablePort } from "./lib/port-picker.js";
 
 const app = express();
 
@@ -8,9 +10,24 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-const port: any = process.env.PORT || 8762;
+const startServer = async () => {
+  // Grab the port dynamically
+  const port = await getAvailablePort(6753, 8762);
 
-ViteExpress.listen(app, port, () =>
-  console.log(`Server is Starting on http://localhost:${port}`),
-);
+  ViteExpress.listen(app, port, () => {
+    console.clear();
+    console.log(
+      chalk.cyan.bold(`
+   _____搁搁_____  ______搁搁搁搁搁   搁搁搁搁搁搁搁搁  搁搁搁搁搁搁搁搁搁
+  搁搁搁搁搁搁搁搁搁搁  搁搁搁搁搁搁搁搁搁搁  搁搁搁搁搁搁搁搁  搁搁搁搁搁搁搁搁搁
+    `),
+    );
+    console.log(
+      chalk.green.bold(` SUCCESS `) +
+        chalk.white(`IRIS-mini Engine active at: `) +
+        chalk.underline.blue(`http://localhost:${port}`),
+    );
+  });
+};
 
+startServer();

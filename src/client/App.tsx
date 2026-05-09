458e94c22171ea06ecdd0c3c0757a8e5cdab845f
Harsh <pandeygautam039@gmail.com>
2026-05-10 04:34:36 +0530

    update App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,4 @@
-// client/src/App.tsx
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Mic, MicOff, Power, Terminal, Zap, ShieldCheck } from "lucide-react";
 
 const App = () => {

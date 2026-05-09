ded32dd17f53cb2af401e614f17933bb0608dae2
Harsh <pandeygautam039@gmail.com>
2026-05-09 12:21:20 +0530

    update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="h-screen w-full bg-black flex items-center justify-center">
+      <h1 className="text-8xl text-white">IRIS-MINi</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;

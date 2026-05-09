f0f802ba46bf3bfce2920669d7a24b18a882a7a2
Harsh <pandeygautam039@gmail.com>
2026-05-09 07:33:21 +0530

    Intial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#09090b",
+        color: "#fff",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1 style={{ letterSpacing: "2px" }}>
+        IRIS-mini <span style={{ color: "#4ade80" }}>NEURAL CORE</span>
+      </h1>
+    </div>
+  );
+};
+
+export default App;

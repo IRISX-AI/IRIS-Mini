b95b0d231b994fb832e5c659674a311442bbf296
Harsh <pandeygautam039@gmail.com>
2026-05-09 20:46:03 +0530

    update .env and main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -7,7 +7,7 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-const port : any = process.env.PORT || 4000;
+const port : any = process.env.PORT || 4450;
 
 ViteExpress.listen(app, port, () =>
   console.log(`Server is Starting on http://localhost:${port}`),

01784937d402ac1e461ff75c0f52bfbae6fb4996
Harsh <pandeygautam039@gmail.com>
2026-05-09 20:17:19 +0530

    update 5 files and delete 16 files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  root: "client",
-  build: {
-    outDir: "../dist/client",
-    emptyOutDir: true, // Cleans the folder before each rebuild
-    watch: {
-      // Ensures the build watcher is sensitive to changes
-      include: 'src/**',
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-    },
-  },
+  plugins: [react()],
 });

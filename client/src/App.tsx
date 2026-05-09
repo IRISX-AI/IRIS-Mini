3638f0962fcc9cfb5b323b8b3c0cee096eaafcab
Harsh <pandeygautam039@gmail.com>
2026-05-09 12:27:37 +0530

    update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="h-screen w-full bg-black flex items-center justify-center">
-      <h1 className="text-8xl text-white">IRIS-MINi</h1>
+      <h1 className="text-8xl text-white">IRIS-MINI</h1>
     </div>
   );
 };

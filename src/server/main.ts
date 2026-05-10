de3e1ffccd1264fff9b648b4c29375397c37b3cd
Harsh <pandeygautam039@gmail.com>
2026-05-10 19:03:25 +0530

    feat: initialize express server with socket.io and vite-express integration

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,17 +1,32 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 import ViteExpress from "vite-express";
 import "../config/dot-env.js";
 import { getAvailablePort } from "./lib/port-picker.js";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+});
 
 const startServer = async () => {
   const port = await getAvailablePort(6754, 8764);
 
-  ViteExpress.listen(app, port, () => {
+  server.listen(port, () => {
     console.clear();
     console.log(`Server is listening on http://localhost:${port}`);
   });
+
+  ViteExpress.bind(app, server);
 };
 
 startServer();

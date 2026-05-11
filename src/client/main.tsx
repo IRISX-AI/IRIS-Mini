601aa35917c4698c2a58dd10316eff723a6b4154
Harsh <pandeygautam039@gmail.com>
2026-05-11 10:44:33 +0530

    feat: install react-toastify and integrate global ToastContainer in main entry point

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,11 +2,25 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Slide, ToastContainer } from "react-toastify";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Slide}
+    />
     <App />
   </React.StrictMode>,
 );

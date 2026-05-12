de6f86245f6518575d8af6cfcf746fbb16547a3a
Harsh <pandeygautam039@gmail.com>
2026-05-12 09:27:32 +0530

    feat: define IRIS_PRODUCTION environment variable and cleanup build script whitespace

--- build.js
+++ build.js
@@ -9,17 +9,17 @@ await esbuild
     format: "esm",
     packages: "external",
     minify: true,
+    define: {
+      "process.env.IRIS_PRODUCTION": '"true"',
+    },
     outfile: "dist/cli.js",
   })
   .catch(() => process.exit(1));
 
 const file = "dist/cli.js";
 let code = fs.readFileSync(file, "utf8");
-
 code = code.replace(/^#!(.*)/gm, "");
-
 code = code.trimStart();
-
 fs.writeFileSync(file, "#!/usr/bin/env node\n" + code);
 
 console.log("CLI Build Complete. Shebang locked to Line 1.");

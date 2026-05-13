099801d267f6968b86cc2972a9b7ca82ff685c84
Harsh <pandeygautam039@gmail.com>
2026-05-13 18:50:07 +0530

    feat: initialize server entry point in main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -87,7 +87,7 @@ const startServer = async () => {
 
     app.use(express.static(__dirname));
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
       res.sendFile(path.join(__dirname, "index.html"));
     });
   } else {
